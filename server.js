32e004bb08a49ea9d5b69e6a151fa0a8a3505767
unknown <darginchik11@gmail.com>
2025-02-22 11:25:29 +0300

    Added CORS to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors'); // Добавьте cors
 const app = express();
+
 app.use(express.json());
+app.use(cors()); // Разрешить запросы с других доменов
 
 let bookings = {};
 
